e4edc66d1771ce7b5cdeec547e52e9e4caa19791
Cody Olsen <stipsan@gmail.com>
2022-07-22 08:37:05 +0200

    super smooth

--- components/CodeSnippetsSetup.tsx
+++ components/CodeSnippetsSetup.tsx
@@ -20,8 +20,14 @@ interface Props {
   state: QuizState
   esmUrl: string
   esmUrlDTS: string
+  esmUrlOrigin: string
 }
-const CodeSnippetsSetup = ({ state, esmUrl, esmUrlDTS }: Props) => {
+const CodeSnippetsSetup = ({
+  state,
+  esmUrl,
+  esmUrlDTS,
+  esmUrlOrigin,
+}: Props) => {
   const lead = useMemo(() => {
     if (state.build === 'sanity build') {
       return (
@@ -43,16 +49,38 @@ const CodeSnippetsSetup = ({ state, esmUrl, esmUrlDTS }: Props) => {
         </>
       )
     }
+
+    if (state.build === 'next build' && state.load === 'build-time') {
+      return (
+        <>
+          Before you can add the import snippet to your
+          <FilenameBadge>
+            sanity.config.{state.typescript ? 'ts' : 'js'}
+          </FilenameBadge>
+          you&#39;ll need to make a few changes to{' '}
+          <FilenameBadge>next.config.js</FilenameBadge> and{' '}
+          <FilenameBadge>
+            pages/_document.{state.typescript ? 'tsx' : 'js'}
+          </FilenameBadge>{' '}
+          .
+        </>
+      )
+    }
+
     return null
-  }, [state.build, state.typescript])
+  }, [state.build, state.load, state.typescript])
 
   const initial = useMemo(() => {
     if (state.build === 'sanity build') {
       return 'sanity.config'
     }
 
+    if (state.build === 'next build' && state.load === 'build-time') {
+      return 'sanity.config'
+    }
+
     return undefined
-  }, [state.build])
+  }, [state.build, state.load])
 
   const files = useMemo(() => {
     if (state.build === 'sanity build') {
@@ -96,16 +124,75 @@ const CodeSnippetsSetup = ({ state, esmUrl, esmUrlDTS }: Props) => {
           ]
     }
 
+    if (state.build === 'next build' && state.load === 'build-time') {
+      return state.typescript
+        ? [
+            {
+              id: 'sanity.config',
+              filename: 'sanity.config.ts',
+              contents: snippet('studio-config-static-import')(
+                snippet('import-static')(JSON5.stringify(esmUrl))
+              ),
+            },
+            {
+              filename: 'next.config.js',
+              contents: snippet('next-config-build-time-ts')(
+                JSON5.stringify(esmUrlOrigin)
+              ),
+            },
+            {
+              id: 'pages/_document',
+              filename: 'pages/_document.tsx',
+              contents: snippet('pages/_document.tsx')(),
+            },
+            {
+              filename: 'themer.d.ts',
+              contents: snippet('themer.d.ts')(JSON5.stringify(esmUrlDTS)),
+            },
+          ]
+        : [
+            {
+              id: 'sanity.config',
+              filename: 'sanity.config.js',
+              contents: snippet('studio-config-static-import')(
+                snippet('import-static')(JSON5.stringify(esmUrl))
+              ),
+            },
+            {
+              filename: 'next.config.js',
+              contents: snippet('next-config-build-time-js')(
+                JSON5.stringify(esmUrlOrigin)
+              ),
+            },
+            {
+              id: 'pages/_document',
+              filename: 'pages/_document.js',
+              contents: snippet('pages/_document.js')(),
+            },
+          ]
+    }
+
     return []
-  }, [esmUrl, esmUrlDTS, state.build, state.typescript])
+  }, [
+    esmUrl,
+    esmUrlDTS,
+    esmUrlOrigin,
+    state.build,
+    state.load,
+    state.typescript,
+  ])
 
   const shouldRender = useMemo(() => {
     if (state.build === 'sanity build') {
       return state.typescript !== null
     }
 
+    if (state.build === 'next build' && state.load === 'build-time') {
+      return state.typescript !== null
+    }
+
     return false
-  }, [state.build, state.typescript])
+  }, [state.build, state.load, state.typescript])
 
   if (!shouldRender) {
     return null

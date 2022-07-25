d881c3455435587ae4acbabf77decf1dd1018e3a
Cody Olsen <stipsan@gmail.com>
2022-07-25 11:28:14 +0200

    fix CORS
    Signed-off-by: Cody Olsen <stipsan@gmail.com>

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ]
+      }
+    ]
+  }
+};
